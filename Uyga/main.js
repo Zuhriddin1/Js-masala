1e9b6ffb191646ae3c1358c94ee60e69d097f5fc
Zuhriddin1 <anarimon1985@gmail.com>
2023-11-25 14:45:15 +0300

    first commit

---
+++ Uyga/main.js
@@ -0,0 +1,184 @@
+// !60 - masala;
+// const score = prompt("Son kiriting");
+// if (score % 2 == 0) {
+//   console.log("Juft");
+// } else {
+//   console.log("Toq");
+// }
+// console.log(score);
+// 62 - masala
+// let arrayString = [1, 2, 3, 4, 5];
+// console.log(arrayString.join(""));
+// 63 - masala;
+// let arr = [1, 2, 3, 4, 5];
+// let arr2 = [6, 7, 8, 9, 10];
+// console.log(arr.join(""), arr2.join(""));
+// 64 - masala;
+// let topIndex = ["hi", "edabit", "fgh", "abc", "fgh"];
+// console.log(topIndex[2]);
+// 67 - masala
+// let arr = [1, 2, 3, 4, 5];
+// if (arr == 1) {
+//   prompt = "son kiriting";
+//   console.log("bir");
+// }
+// if (arr == 2) {
+//   prompt = "Son";
+//   console.log("ikki");
+// }
+// if (arr == 3) {
+//   prompt = "Son";
+//   console.log("uch");
+// }
+// if (arr == 4) {
+//   prompt = "Son";
+//   console.log("tort");
+// }
+// if (arr == 5) {
+//   prompt = "Son";
+//   console.log("besh");
+// }
+// console.log(`arr`);
+// 69 - masala;
+//80-misol
+// let a=prompt("sozni kiriting")
+// if( a=="happy"){
+//     console.log(("Today, I am feeling happy")+"    ");
+// }else if(a=="sad"){
+//     console.log("Today, I am feeling sad" +"");
+//     }else{
+//         console.log("Today, I am feeling neutral"+"");
+//     }
+
+//81-misol
+// let a=prompt("sozni kiriting")
+// function bogin(a) {
+//     if(a=="he"){
+//         console.log(he+1);
+//     }else if(a=="bo"){
+//         console.log(bo+1);
+//     }else if(a=="Na"){
+// console.log(Na+1);
+//     }else{
+//         console.log("Koda hatolik");
+//     }
+// }
+// console.log(bogin());
+
+//82 misolni hali otmadik
+//83-misol
+// let a=prompt("a ni kiriting")
+// let b=prompt("b ni kiriting")
+//     if(a=="hello",b=="hELLo"){
+//         console.log(true);
+//     }else if(a=="motive", b=="emotive"){
+// console.log(false);
+//     }else if(a=="venom",b=="VENOM"){
+//         console.log(true);
+//     }else if(a=="mask",b=="mAskinG"){
+//         console.log(false);
+//     }else{
+//         console.log("kadagi xatolik");
+//     }
+//85-masala
+// let a=30000;
+// let b=25000;
+// let c=390205;
+// result=(a-b)/1000;
+// result1=c/1000;
+// result2=result1/result
+// console.log(Math.ceil(result2));
+//86-masala
+// let masala=[a=1,b=86,c=-5]
+// masala=[a*2,b*2,c*2]
+// console.log(masala);
+//87-masala
+// let a=prompt("sozlarni kiriting")
+// console.log([a.length]);
+//88-masala
+// let km=prompt("pulni kiriting")
+// if(km >=1000 && km < 4000){
+//     console.log(1+"km");
+// }else if(km >=4000 && km < 5000){
+//     console.log(2+"km");
+// }else if(km >=5000 && km < 6000){
+//     console.log(3+"km");
+// }else if(km >=6000 && km < 8000){
+//     console.log(4+"km");
+// }else{
+//     console.log("pul hato kiritilgan boshqatan urunib koring");
+// }
+//89-masala
+// let a=prompt("sozni kiriting")
+// result=d==1
+//   console.log(a.indexOf(result + 1))
+//90-masala
+// let boshjoy=prompt("sozni kiriting")
+// console.log(boshjoy.split(""));
+//91 masala
+// let a=prompt("a ni kiriting")
+// let b=prompt("b ni kiriting")
+// if(b==8){
+// console.log(b*3);
+// }else if(b==3){
+// console.log(b*33);
+// }else if(b==11){
+// console.log(b*2);
+// }else{
+//     console.log("kod da xatolik boshqatan urinib korin");
+// }
+//93-misol
+// function repeatString(string, number) {
+//     let result = "";
+//         for (let i = 0; i < number; i++) {
+//         result += string;
+//     }
+//         return result;
+// }
+// let output = repeatString("go", 3);
+// console.log(output);
+// 94 masala
+/*function summa([]){
+    if( 5 < num ){
+        let res = num + ( 5 < num);
+        console.log(res);
+    }else{
+        console.log("not");
+    }
+}
+summa( [20, 5 ,  7 ,10 , 20])*/
+
+//95-misol
+// let masala=[a=2,b=7,c=4,]
+// result=a+b+c
+// console.log(result);
+//97-misol
+/*let masala=[a=-1,b=-4,c=-3,d=-2]
+result=a+b+c+d
+console.log(result);
+98-misol
+function summa(a, b , c){
+    let res = a + b
+    if(res > c ){
+        console.log(true);
+    } else{
+        console.log(false);
+    }
+}
+summa(4, 6, 9)*/
+// let a=7
+// if(a==3){
+//     console.log(1+2+3);
+// }else if(a==10){
+//     console.log(1+2+3+4+5+6+7+8+9+10);
+// }else if(a==7){
+//     console.log(1+2+3+4+5+6+7);
+// }else{
+//     console.log("xato");
+// }
+// 107-misol
+// *let arr=[1,2,3]
+// let boshi=arr[0]
+// arr[0]=arr[arr.length -1];
+// arr[arr.length-1]=boshi;
+// console.log(arr);
---
+++ main.js
@@ -0,0 +1,47 @@
+// array
+// let arr = [1, 2, 3, 4, 5]; //index
+// console.log(arr[0]);
+// arr[100] = 100;
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr(arr.length - 1));
+// let arr = [1, 2, 3, 4, 5];
+// let boshi = arr[0];
+// arr[0] = arr[arr.length - 1];
+// arr[arr.length - 1] = boshi;
+// console.log(arr);
+// let arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+// let egindi = 0;
+// let arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr.length; i++) {
+//   egindi = egindi + arr[i];
+// }
+// console.log(egindi);
+// let arr = [1, 2, 3, 4, 5];
+// arr.push(1000020000000);
+// console.log(arr);
+// let arr = [1, 2, 3, 4, 5];
+// arr.unshift(0);
+// console.log(arr);
+// let arr = [1, 2, 3, 4, 5];
+// arr.pop();
+// console.log(arr);
+// let arr;
+// for (let i = 0; i < 5; i++) {
+//   let x = +prompt("Son");
+//   arr.push(x);
+// }
+// let word = `Salom,dunyo`;
+// console.log(word.split(","));
+// let word = ["Salom", "Fuck", "you"];
+// console.log(word.join(" "));
+// let word = prompt("Soz");
+// let arr = gap.split("");
+// for (let i = 0; i < arr.length; i++) {   
+// if (arr[i] == "a") {
+// }
+// }
+// console.log(word);
